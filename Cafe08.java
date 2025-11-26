a49baa55d7c8b2ff98fdd043d2f0134a51b3f21e
kamilakhairunnisa <erinazkk@gmail.com>
2025-11-27 02:11:17 +0700

    cafe08

---
+++ Cafe08.java
@@ -0,0 +1,18 @@
+public class Cafe08 {
+    
+    public static void Menu() {
+    System.out.println("==== MENU RESTO KAFE");
+    System.out.println("1. Kopi Hitam - RP 15,000");
+    System.out.println("2. Cappucino - RP 20,000");
+    System.out.println("3. Latte - RP 22,000");
+    System.out.println("4. Teh Tarik - RP 12,000");
+    System.out.println("5. Roti Bakar - RP 10,000");
+    System.out.println("6. Mie Goreng - RP 18,000");
+    System.out.println("===========================");
+    System.out.println("Silahkan pilih menu yang anda inginkan");      
+    }
+
+    public static void main(String[] args) {
+        Menu();     
+    }
+}
