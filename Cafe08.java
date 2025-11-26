5e71c0c29f80b2abca8aa009f57014b645caef64
kamilakhairunnisa <erinazkk@gmail.com>
2025-11-27 03:14:03 +0700

    modifikasi percobaan 2 pertanyaan 6

--- Cafe08.java
+++ Cafe08.java
@@ -1,11 +1,20 @@
 public class Cafe08 {
     
-    public static void Menu(String namaPelanggan, boolean isMember) {
+    public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
         System.out.println("Selamat datang, " + namaPelanggan + "!");
 
         if (isMember) {
             System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
         }
+
+        if (kodePromo.equals("DISKON50")) {
+            System.out.println("Kode promo valid: Anda mendapatkan diskon 50%!");
+        } else if (kodePromo.equals("DISKON30")) {
+            System.out.println("Kode promo valid: Anda mendapatkan diskon 30%!");
+        } else {
+            System.out.println("Kode promo invalid atau tidak ada promo yang berlaku.");
+        }
+        
     System.out.println("==== MENU RESTO KAFE");
     System.out.println("1. Kopi Hitam - RP 15,000");
     System.out.println("2. Cappucino - RP 20,000");
@@ -18,6 +27,6 @@ public class Cafe08 {
     }
 
     public static void main(String[] args) {
-        Menu("Andi", true);     
+        Menu("Andi", true, "") ;     
     }
 }
