06117d5c7028dce3800e9c5ac4de86a9c9df3810
kamilakhairunnisa <erinazkk@gmail.com>
2025-11-27 05:16:15 +0700

    PengunjungCafe-Modifikasi

--- PengunjungCafe08.java
+++ PengunjungCafe08.java
@@ -2,13 +2,14 @@ public class PengunjungCafe08 {
 
     static void daftarPengunjung(String... namaPengunjung) {
         System.out.println("Daftar Nama Pengunjung:");
-        for (int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("- " + namaPengunjung[i]);
+        for (String nama : namaPengunjung) {
+            System.out.println("- " + nama);
         }
     }
 
     public static void main(String[] args) {
         daftarPengunjung("Ali", "Budi", "Citra");
+
         daftarPengunjung("Andi", "Eko");
         daftarPengunjung("Doni", "Eti", "Fahmi", "Galih");
     }
