7f188f346757b83a848acdaec1ff7d88a35f4cc9
kamilakhairunnisa <erinazkk@gmail.com>
2025-11-27 14:56:01 +0700

    modifikasi tugas3

--- RekapPenjualanCafe08.java
+++ RekapPenjualanCafe08.java
@@ -3,92 +3,64 @@ import java.util.Scanner;
 
 public class RekapPenjualanCafe08 {
 
-    static String[] menu = {"Kopi", "Teh", "Es Kelapa Muda", "Roti Bakar", "Gorengan"};
-    static int[][] penjualan = new int [5][7];
+    static String[] menu;
+    static int[][] penjualan;
+    static int jumlahMenu, jumlahHari;
 
 
     static void inputData() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("=== Data Penjualan Cafe ===");
+        System.out.print("Masukkan jumlah menu: ");
+        jumlahMenu = sc.nextInt();
+        sc.nextLine();  
 
-        for (int i = 0; i < menu.length; i++) {
-            System.out.println("Menu " + menu[i]);
-            for (int hari = 0; hari < 7; hari++) {
-                System.out.print(" Hari ke- " + (hari + 1) + " : ");
-                penjualan[i] [hari] = sc.nextInt();
+        System.out.print("Masukkan jumlah hari penjualan: ");
+        jumlahHari = sc.nextInt();
+        sc.nextLine();
+
+        menu = new String[jumlahMenu];
+        penjualan = new int[jumlahMenu][jumlahHari];
+
+        System.out.println("\n=== Input Data Menu ===");
+        for (int i = 0; i < jumlahMenu; i++) {
+            System.out.print("Nama menu ke-" + (i + 1) + ": ");
+            menu[i] = sc.nextLine();
+        }
+
+        System.out.println("\n=== Input Data Penjualan ===");
+        for (int i = 0; i < jumlahMenu; i++) {
+            System.out.println("Menu: " + menu[i]);
+            for (int hari = 0; hari < jumlahHari; hari++) {
+                System.out.print(" Hari ke-" + (hari + 1) + " : ");
+                penjualan[i][hari] = sc.nextInt();
             }
         }
     }
 
-    static void tampilData() { 
-        System.out.println("\n=== Tabel Data Penjualan 7 Hari ==="); 
-         System.out.printf("%-15s", "Menu");
-    for (int h = 1; h <= 7; h++) {
-        System.out.printf("%5s", "H" + h);
-    }
-    System.out.println();
+    static void tampilData() {
+        System.out.println("\n=== Tabel Penjualan ===");
 
-    for (int i = 0; i < menu.length; i++) { 
-        System.out.printf("%-15s", menu[i]);
-        for (int j = 0; j < 7; j++) { 
-            System.out.printf("%5d", penjualan[i][j]);
+        System.out.printf("%-15s", "Menu");
+        for (int h = 1; h <= jumlahHari; h++) {
+            System.out.printf("%5s", "H" + h);
         }
-        System.out.println(); 
+        System.out.println();
+
+        for (int i = 0; i < jumlahMenu; i++) {
+            System.out.printf("%-15s", menu[i]);
+            for (int j = 0; j < jumlahHari; j++) {
+                System.out.printf("%5d", penjualan[i][j]);
+            }
+            System.out.println();
+        }
     }
-}
+
     static void tampilMenuTerlaris() {
         System.out.println("\n=== Menu Terlaris per Hari ===");
-        for (int hari = 0; hari < 7; hari++) {
-        int max = -1;
-        String namaMenu = "";
-
-        for (int i = 0; i < menu.length; i++) {
-            if (penjualan[i][hari] > max) {
-                max = penjualan[i][hari];
-                namaMenu = menu[i];
-            }
-        }
-    System.out.println("Hari ke-" + (hari + 1) + ": " + namaMenu + " (" + max + ")");
-        }
-    }
-
-    static void tampilMenuTersepi() {
-        System.out.println("\n=== Menu tersepi dalam 7 hari ===");
-        int min = Integer.MAX_VALUE;
-        String menuMin = "";
-
-        for (int i = 0; i < menu.length; i++) {
-            int total = 0;
-            for (int j = 0; j <7; j++) {
-                total += penjualan[i][j];
-            }
-            if (total < min) {
-                min = total;
-                menuMin = menu[i];
-            }
-        }
-        System.out.println("Menu dengan total penjualan terendah: " + menuMin + "" + min);
-    }
-
-
-    static void rataRataPerMenu() {
-        System.out.println("\n=== Rata-rata Penjualan per Menu ===");
-        for (int i = 0; i < menu.length; i++) {
-            int total = 0;
-            for (int j = 0; j < 7; j++) {
-                total += penjualan[i][j];
-            }
-            int rata = (int) total / 7;
-            System.out.println(menu[i] + ": " + rata);
-        }
-    }
-
-    static void menuTerlarisPerHari() {
-        System.out.println("\nMenu Terlaris per Hari:");
-        for (int hari = 0; hari < 7; hari++) {
-            int max = 0;
+        for (int hari = 0; hari < jumlahHari; hari++) {
+            int max = -1;
             String namaMenu = "";
-            for (int i = 0; i < menu.length; i++) {
+            for (int i = 0; i < jumlahMenu; i++) {
                 if (penjualan[i][hari] > max) {
                     max = penjualan[i][hari];
                     namaMenu = menu[i];
@@ -98,12 +70,42 @@ public class RekapPenjualanCafe08 {
         }
     }
 
+    static void tampilMenuTersepi() {
+        System.out.println("\n=== Menu Tersepi Selama Periode ===");
+        int min = Integer.MAX_VALUE;
+        String menuMin = "";
+
+        for (int i = 0; i < jumlahMenu; i++) {
+            int total = 0;
+            for (int j = 0; j < jumlahHari; j++) {
+                total += penjualan[i][j];
+            }
+            if (total < min) {
+                min = total;
+                menuMin = menu[i];
+            }
+        }
+        System.out.println("Menu dengan total terendah: " + menuMin + " (" + min + ")");
+    }
+
+    static void rataRataPerMenu() {
+        System.out.println("\n=== Rata-rata Penjualan per Menu ===");
+        for (int i = 0; i < jumlahMenu; i++) {
+            int total = 0;
+            for (int j = 0; j < jumlahHari; j++) {
+                total += penjualan[i][j];
+            }
+            double rata = total / (double) jumlahHari;
+            System.out.println(menu[i] + ": " + rata);
+        }
+    }
+
     static void menuTersepiPerHari() {
-        System.out.println("\nMenu Tersepi per Hari:");
-        for (int hari = 0; hari < 7; hari++) {
+        System.out.println("\n=== Menu Tersepi per Hari ===");
+        for (int hari = 0; hari < jumlahHari; hari++) {
             int min = Integer.MAX_VALUE;
             String namaMenu = "";
-            for (int i = 0; i < menu.length; i++) {
+            for (int i = 0; i < jumlahMenu; i++) {
                 if (penjualan[i][hari] < min) {
                     min = penjualan[i][hari];
                     namaMenu = menu[i];
@@ -119,8 +121,6 @@ public class RekapPenjualanCafe08 {
         tampilMenuTerlaris();
         tampilMenuTersepi();
         rataRataPerMenu();
-        menuTerlarisPerHari();
         menuTersepiPerHari();
     }
 }
-
